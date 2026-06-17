58da4b4c63fed48ca5f91f376226256385139285
prathmesh8857 <butteprathamesh077@gmail.com>
2026-06-17 09:51:32 +0530

    operators,logical operators,arithmatic operators

---
+++ Main.java
@@ -0,0 +1,57 @@
+public class Main {
+
+//     //public static void main(String[] args) {
+//         //int num1 = 6;
+//         //int num2 = 2;
+//         //int result = num1+num2;
+//         //System.out.println("sum is :" + result);
+
+//             //arithmatic operators
+//             // System.out.println(num1 + num2);
+//             // System.out.println(num1 - num2);
+//             // System.out.println(num1 * num2);
+//             // System.out.println(num1 / num2);
+//             // System.out.println(num1 % num2);
+
+//             //comparison operators
+//             //System.out.println(num1 > num2);
+//             //System.out.println(num1 < num2);
+//             //System.out.println(num1 >= num2);
+//             //System.out.println(num1 <= num2);
+//             //System.out.println(num1 != num2);
+
+        
+//     }
+// }
+
+    public static void main(String[] args){
+
+        // boolean isTrue = true;
+        // boolean isFalse = false;
+        
+        //     if (isTrue && isFalse) {
+        //         System.out.println("True");
+        // }else{
+        //     System.out.println("False");
+        // }
+
+        // if(isTrue && isFalse) {
+        //         System.out.println("True");
+        // }else{
+        //     System.out.println("False");
+        // }
+
+
+
+        int num = 7;
+        //num++;
+        System.out.println(++num);
+        System.out.println(num);
+        //++num
+        //num--
+        //--num
+
+    }
+}
+    
+
