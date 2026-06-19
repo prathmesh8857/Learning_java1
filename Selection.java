2a9b5de2bf69e0d4366e79a011bdd71b3170eed6
prathmesh8857 <butteprathamesh077@gmail.com>
2026-06-19 09:46:25 +0530

    ittetations,incrimentanddecriment

--- Selection.java
+++ Selection.java
@@ -1,3 +1,5 @@
+import java.time.DayOfWeek;
+
 public class Selection {
     
     public static void main(String[] args) {
@@ -52,12 +54,68 @@ public class Selection {
         // String result = (age >= 18) ? "Adult" : "Minor";
         // System.out.println(result);
 
-        int marks = 75;
-        String grade = (marks >= 90) ? "A" //after question mark value is true
-                    : (marks >= 80) ? "B"  //before colen values are false
-                    :(marks >= 70) ? "C"
-                    :(marks >= 60) ? "d" : "F";
-        System.out.println("Grade: " + grade);           
+        // int marks = 75;
+        // String grade = (marks >= 90) ? "A" //after question mark value is true
+        //             : (marks >= 80) ? "B"  //before colen values are false
+        //             :(marks >= 70) ? "C"
+        //             :(marks >= 60) ? "d" : "F";
+        // System.out.println("Grade: " + grade);  
+        
+        
+        //switch case
+        
+        // int day = 2;
+        // switch (day) {
+        //     case 1:
+        //         System.out.println("Monday");
+        //         break;
+        //     case 2:
+        //         System.out.println("Tuesday");
+        //         break;
+        //     case 3:
+        //         System.out.println("Wednesday");
+        //         break;
+        //     case 4:
+        //         System.out.println("Thursday");
+        //         break;
+        //     case 5:
+        //         System.out.println("Friday");
+        //         break;
+        //     case 6:
+        //         System.out.println("Saturday");
+        //         break;
+        //     case 7:
+        //         System.out.println("Sonday");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid Day");
+        //         break;
+        // }
+
+
+        // int value = 5;
+
+        // switch (value) {
+        //     case 1 ->
+        //     System.out.println("Value was 1");
+        //     case 2,3 -> 
+        //     System.out.println("Value was 2 or 3");
+        //     default -> 
+        //     System.out.println("Value was not 1,2 or 3");
+            
+        // }
+
+        //switch as expression
+        int value = 5;
+
+        int result = switch (value) {
+            case 1 ->
+            1;
+            case 2,3 ->
+            2;
+            default ->
+            0;
+        };
 
 
     }
---
+++ incrimentdicriment.java
@@ -0,0 +1,39 @@
+public class incrimentdicriment {
+
+    public static void main(String[] args) {
+
+        //incriment
+        // for (int i = 1 ; i <= 10; i++){
+        //     System.out.println(i);
+        // }
+
+        //decriment
+        //noly while loop
+        // int sec = 10
+        // while (sec > 0) {
+        //     System.out.println(sec);
+        //     sec--;
+            
+        // }
+
+        //do while loop
+        // int sec = 10;
+        // do{
+        //     System.out.println(sec);
+        //     sec--;
+        // }
+
+        // while (sec > 0);
+            
+        //jump statement 
+        for(int i = 0; i < 10; i++) {
+            if(i == 3) {
+                continue;
+            }
+            System.out.println("Hellow world " + i);
+        }
+
+
+    }
+    
+}
